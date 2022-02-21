dd9b2fe97188a275afdf9b04a2a431aa58462a45
Adrian Młynarek <adml@grapeup.com>
2022-02-21 12:29:53 +0100

    Blank Spring Boot project initialization

---
+++ src/main/java/com/memsource/projectdisplay/ProjectDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.memsource.projectdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDisplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memsource/projectdisplay/ProjectDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memsource.projectdisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
